35d20d021f1184ca5321e187687ad7a0f21f05ad
leibrother <leibrother@foxmail.com>
2025-09-21 21:47:37 +0800

    通过启动参数加载配置文件

--- qqbot-example/src/main/java/cc/rapidev/qqbot/example/Main.java
+++ qqbot-example/src/main/java/cc/rapidev/qqbot/example/Main.java
@@ -1,7 +1,15 @@
 package cc.rapidev.qqbot.example;
 
 import cc.rapidev.qqbot.Bot;
+import cc.rapidev.qqbot.BotConfig;
 import cc.rapidev.qqbot.common.Constant;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.stream.Stream;
 
 /**
  * @author leibrother
@@ -19,9 +27,31 @@ public class Main {
         System.out.println();
     }
 
+    public static Properties getPropertiesByArgs(String[] args) {
+        Properties properties = new Properties();
+        if (args != null) {
+            Optional<String> optional = Stream.of(args).filter(arg -> arg.startsWith("--config=")).findFirst();
+            if (optional.isPresent()) {
+                String path = optional.get().split("=")[1];
+                File file = new File(path);
+                if (file.exists()) {
+                    try {
+                        properties.load(new FileInputStream(file));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+        return properties;
+    }
+
     public static void main(String[] args) {
         printWelcome();
-        Bot bot = new Bot();
+        Properties properties = getPropertiesByArgs(args);
+        BotConfig config = BotConfig.create();
+        config.loadProperties(properties);
+        Bot bot = new Bot(config);
         bot.run(true);
     }
 
