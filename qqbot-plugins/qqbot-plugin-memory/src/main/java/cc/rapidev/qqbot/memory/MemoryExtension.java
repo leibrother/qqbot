c640794075dc3e4611d3b70b9d7e9b69935ec1ca
leibrother <leibrother@foxmail.com>
2026-05-22 16:34:50 +0800

    打包调整

---
+++ qqbot-plugins/qqbot-plugin-memory/src/main/java/cc/rapidev/qqbot/memory/MemoryExtension.java
@@ -0,0 +1,20 @@
+package cc.rapidev.qqbot.memory;
+
+import cc.rapidev.qqbot.Bot;
+import cc.rapidev.qqbot.extension.Extension;
+
+/**
+ * @author leibrother
+ */
+public class MemoryExtension implements Extension {
+
+    public MemoryExtension(Bot bot) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
