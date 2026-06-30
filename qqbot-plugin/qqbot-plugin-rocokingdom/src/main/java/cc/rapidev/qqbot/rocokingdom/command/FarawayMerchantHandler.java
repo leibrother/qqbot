a401c5da8c4a497090a0b8d11807530c8a74b7d6
leibrother <leibrother@foxmail.com>
2026-07-01 00:46:56 +0800

    添加时间检查，早上8点前直接返回，不进行接口调用

--- qqbot-plugin/qqbot-plugin-rocokingdom/src/main/java/cc/rapidev/qqbot/rocokingdom/command/FarawayMerchantHandler.java
+++ qqbot-plugin/qqbot-plugin-rocokingdom/src/main/java/cc/rapidev/qqbot/rocokingdom/command/FarawayMerchantHandler.java
@@ -1,6 +1,7 @@
 package cc.rapidev.qqbot.rocokingdom.command;
 
 import cc.rapidev.qqbot.Bot;
+import cc.rapidev.qqbot.api.model.Message;
 import cc.rapidev.qqbot.common.request.Requester;
 import cc.rapidev.qqbot.extension.command.Command;
 import cc.rapidev.qqbot.extension.command.CommandHandler;
@@ -8,6 +9,8 @@ import cc.rapidev.qqbot.message.MessageContext;
 import cc.rapidev.qqbot.rocokingdom.view.FarawayMerchantView;
 import com.fasterxml.jackson.databind.JsonNode;
 import okhttp3.HttpUrl;
+
+import java.time.LocalTime;
 
 /**
  * @author leibrother
@@ -22,6 +25,10 @@ public class FarawayMerchantHandler implements CommandHandler {
 
     @Override
     public void handle(MessageContext context, Command command) {
+        if (LocalTime.now().isBefore(LocalTime.of(8, 0))) {
+            context.reply(Message.text("远行商人已经休息了哦，请早上8点后再来吧~"));
+            return;
+        }
         JsonNode data = data();
         JsonNode rounds = data.get("rounds");
         int index = rounds.size() - 1;
