52ddf693aedb238c0ca557581086025c2c24bedc
leibrother <leibrother@foxmail.com>
2026-07-03 18:01:55 +0800

    同步完成添加提示回复

--- qqbot-plugin/qqbot-plugin-rocokingdom/src/main/java/cc/rapidev/qqbot/rocokingdom/command/RocokingdomDexHandler.java
+++ qqbot-plugin/qqbot-plugin-rocokingdom/src/main/java/cc/rapidev/qqbot/rocokingdom/command/RocokingdomDexHandler.java
@@ -1,7 +1,10 @@
 package cc.rapidev.qqbot.rocokingdom.command;
 
+import cc.rapidev.qqbot.api.model.Message;
+import cc.rapidev.qqbot.extension.command.Command;
 import cc.rapidev.qqbot.extension.command.CommandHandlerSet;
 import cc.rapidev.qqbot.extension.command.Keyword;
+import cc.rapidev.qqbot.message.MessageContext;
 import cc.rapidev.qqbot.rocokingdom.command.dex.QueryHandler;
 import cc.rapidev.qqbot.rocokingdom.service.RocokingdomDexService;
 
@@ -15,7 +18,12 @@ public class RocokingdomDexHandler extends CommandHandlerSet {
     public RocokingdomDexHandler(RocokingdomDexService service) {
         this.service = service;
         super(new QueryHandler(service));
-        add(new Keyword("同步", "同步数据"), (_, _) -> this.service.sync());
+        add(new Keyword("同步", "同步数据"), this::sync);
+    }
+
+    private void sync(MessageContext context, Command command) {
+        this.service.sync();
+        context.reply(Message.text("同步完成"));
     }
 
 }
