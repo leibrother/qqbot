78f426329adfb75910f1d1ec3753e79917bdc5b3
leibrother <leibrother@foxmail.com>
2026-07-03 17:58:24 +0800

    修复发送给用户数据不是当前时间段的轮次的问题

--- qqbot-plugin/qqbot-plugin-rocokingdom/src/main/java/cc/rapidev/qqbot/rocokingdom/command/FarawayMerchantHandler.java
+++ qqbot-plugin/qqbot-plugin-rocokingdom/src/main/java/cc/rapidev/qqbot/rocokingdom/command/FarawayMerchantHandler.java
@@ -9,7 +9,11 @@ import cc.rapidev.qqbot.rocokingdom.view.FarawayMerchantView;
 import com.fasterxml.jackson.databind.JsonNode;
 import okhttp3.HttpUrl;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.Iterator;
 
 /**
  * @author leibrother
@@ -24,16 +28,24 @@ public class FarawayMerchantHandler implements CommandHandler {
 
     @Override
     public void handle(MessageContext context, Command command) {
-        if (LocalTime.now().isBefore(LocalTime.of(8, 0))) {
+        LocalDateTime now = LocalDateTime.now();
+        if (now.toLocalTime().isBefore(LocalTime.of(8, 0))) {
             context.reply(Message.text("远行商人已经休息了哦，请早上8点后再来吧~"));
             return;
         }
         JsonNode data = data();
-        JsonNode rounds = data.get("rounds");
-        int index = rounds.size() - 1;
-        JsonNode round = rounds.get(index);
-        FarawayMerchantView view = new FarawayMerchantView(round);
-        context.reply(view.render());
+        Iterator<JsonNode> rounds = data.get("rounds").elements();
+        while (rounds.hasNext()) {
+            JsonNode round = rounds.next();
+            LocalDateTime startTime = Instant.ofEpochMilli(round.get("startTime").longValue()).atZone(ZoneOffset.of("+8")).toLocalDateTime();
+            LocalDateTime endTime = Instant.ofEpochMilli(round.get("endTime").longValue()).atZone(ZoneOffset.of("+8")).toLocalDateTime();
+            if (now.isAfter(startTime) && now.isBefore(endTime)) {
+                FarawayMerchantView view = new FarawayMerchantView(round);
+                context.reply(view.render());
+                return;
+            }
+        }
+        context.reply(Message.text("远行商人数据未更新，请稍等片刻"));
     }
 
     private JsonNode data() {
